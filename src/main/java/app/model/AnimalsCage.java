57cd7217de3cbc6fbbecd6ea34ed9904e6fc6c99
Andrey Pavlov <andr.pavlov@zebrains.team>
2024-07-13 15:41:03 +0300

    add qualifire

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -11,17 +11,15 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
     private Timer timer;
 
-    AnimalsCage() {
-        timer = new Timer();
-    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
